2afbe7501ebf0c10d597e8ec2aa508c9ccd33961
Alexander <ivanov.unn@gmail.com>
2021-10-11 14:53:28 +0300

    Fix task2 not loading csvtojson properly.

--- dist/task2.js
+++ dist/task2.js
@@ -1,19 +1,64 @@
-"use strict";
+'use strict';
 
-require("core-js/modules/web.dom-collections.iterator.js");
+require('core-js/modules/web.dom-collections.iterator.js');
 
-var fs = _interopRequireWildcard(require("fs"));
+var fs = _interopRequireWildcard(require('fs'));
 
-var csv = _interopRequireWildcard(require("csvtojson"));
+var csv = require('csvtojson');
 
-var _stream = require("stream");
+var _stream = require('stream');
 
-function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+function _getRequireWildcardCache(nodeInterop) {
+  if (typeof WeakMap !== 'function') return null;
+  var cacheBabelInterop = new WeakMap();
+  var cacheNodeInterop = new WeakMap();
+  return (_getRequireWildcardCache = function _getRequireWildcardCache(
+    nodeInterop
+  ) {
+    return nodeInterop ? cacheNodeInterop : cacheBabelInterop;
+  })(nodeInterop);
+}
 
-function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+function _interopRequireWildcard(obj, nodeInterop) {
+  if (!nodeInterop && obj && obj.__esModule) {
+    return obj;
+  }
+  if (obj === null || (typeof obj !== 'object' && typeof obj !== 'function')) {
+    return { default: obj };
+  }
+  var cache = _getRequireWildcardCache(nodeInterop);
+  if (cache && cache.has(obj)) {
+    return cache.get(obj);
+  }
+  var newObj = {};
+  var hasPropertyDescriptor =
+    Object.defineProperty && Object.getOwnPropertyDescriptor;
+  for (var key in obj) {
+    if (key !== 'default' && Object.prototype.hasOwnProperty.call(obj, key)) {
+      var desc = hasPropertyDescriptor
+        ? Object.getOwnPropertyDescriptor(obj, key)
+        : null;
+      if (desc && (desc.get || desc.set)) {
+        Object.defineProperty(newObj, key, desc);
+      } else {
+        newObj[key] = obj[key];
+      }
+    }
+  }
+  newObj.default = obj;
+  if (cache) {
+    cache.set(obj, newObj);
+  }
+  return newObj;
+}
 
-(0, _stream.pipeline)(fs.createReadStream('./csv/input.csv'), csv({
-  delimiter: ';'
-}), fs.createWriteStream('./csv_to_txt/output.txt', 'utf8'), err => {
-  if (err) console.log(err);
-});
+(0, _stream.pipeline)(
+  fs.createReadStream('./csv/input.csv'),
+  csv({
+    delimiter: ';',
+  }),
+  fs.createWriteStream('./csv_to_txt/output.txt', 'utf8'),
+  err => {
+    if (err) console.log(err);
+  }
+);
